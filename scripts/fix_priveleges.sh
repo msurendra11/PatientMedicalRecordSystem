532560529f119ef084d6b7104b1ab8f1585449f7
msurendra11 <140495172+msurendra11@users.noreply.github.com>
2023-09-08 12:53:03 +0530

    Add files via upload

---
+++ scripts/fix_priveleges.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+chmod +x /home/ec2-user/server/* jar
+chmod +x /home/ec2-user/server/server_clear.sh
+chmod +x /home/ec2-user/server/server_start.sh
+chmod +x /home/ec2-user/server/server_stop.sh
+
+
