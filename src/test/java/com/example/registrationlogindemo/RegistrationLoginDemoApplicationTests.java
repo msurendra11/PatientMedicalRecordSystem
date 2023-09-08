94fdd655f504c8a92c7f52abe3b59d759615b858
msurendra11 <140495172+msurendra11@users.noreply.github.com>
2023-09-08 15:00:44 +0530

    Update RegistrationLoginDemoApplicationTests.java

--- src/test/java/com/example/registrationlogindemo/RegistrationLoginDemoApplicationTests.java
+++ src/test/java/com/example/registrationlogindemo/RegistrationLoginDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RegistrationLoginDemoApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
