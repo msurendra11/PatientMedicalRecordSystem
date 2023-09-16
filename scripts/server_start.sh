5216e85b82309b54a0a753dec80bf56bd1d89ce9
msurendra11 <140495172+msurendra11@users.noreply.github.com>
2023-09-16 21:50:47 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 aws s3 cp s3://mrs-codedeploy/patient-registration/target/patient-registration-0.0.1.jar /home/ec2-user/server --sse aws:kms --sse-kms-key-id arn:aws:kms:us-east-2:639384053590:alias/aws/s3
-sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar patient-registration-0.0.1.jar
