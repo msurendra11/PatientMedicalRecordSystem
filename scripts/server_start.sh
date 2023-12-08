f940f6f7759a17a2316d0b9ed84276c36c5f55b2
msurendra11 <140495172+msurendra11@users.noreply.github.com>
2023-12-08 18:49:27 +0530

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-aws s3 cp s3://mrs-code-deploy/patient-registration/target/patient-registration-0.0.1.jar /home/ec2-user/server
+aws s3 cp s3://iqe-poc-bucket/patient-registration/target/patient-registration-0.0.1.jar /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
     patient-registration-0.0.1.jar > /dev/null 2> /dev/null < /dev/null &
